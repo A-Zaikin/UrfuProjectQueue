c3975bcc5c4d87caf44d846b11b6941ced561584
Alexander Zaikin <ay.zaikin@gmail.com>
2021-12-25 16:06:35 +0500

    Добавлено скачивание файла (не работает)

--- ProjectQueue/ProjectQueue/MainPage.xaml.cs
+++ ProjectQueue/ProjectQueue/MainPage.xaml.cs
@@ -4,22 +4,72 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Net.Http;
+
 using Xamarin.Forms;
+using Xamarin.Forms.PlatformConfiguration;
 
 namespace ProjectQueue
 {
     public partial class MainPage : ContentPage
     {
+        private string debugLabel;
+        public string DebugLabel
+        {
+            get { return debugLabel; }
+            set
+            {
+                debugLabel = value;
+                OnPropertyChanged(nameof(DebugLabel)); // Notify that there was a change on this property
+            }
+        }
+
         public MainPage()
         {
             InitializeComponent();
+            BindingContext = this;
+
+            DebugLabel = "New label text";
         }
 
-        void Entry_Completed(object sender, EventArgs e)
+        void EntryCompleted(object sender, EventArgs e)
         {
-            var entry = ((Entry)sender);
+            var entry = (Entry)sender;
             var text = entry.Text;
-            DisplayAlert("Debug", text, "OK");
+            DebugLabel = text;
+            var downloadUrl = FormDownloadUrl(text);
+            DebugLabel = downloadUrl;
+            //var resultTask = DownloadXlsxFile(downloadUrl);
+            //resultTask.Wait();
+            //DebugLabel = resultTask.Result;
+        }
+
+        string FormDownloadUrl(string inputUrl)
+        {
+            var re = new Regex(@"\/d\/(.+)\/");
+            var match = re.Match(inputUrl);
+            var id = match.Groups[1].Value;
+            var downloadUrl = $"https://docs.google.com/spreadsheets/d/{id}/export?format=xlsx&id={id}";
+            return downloadUrl;
+        }
+
+        async Task<string> DownloadXlsxFile(string url)
+        {
+            var client = new HttpClient();
+            var request = new HttpRequestMessage(HttpMethod.Get, new Uri(url));
+
+            var response = await client.SendAsync(request);
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                var responseContent = response.Content;
+                var json = await responseContent.ReadAsStringAsync();
+                return json;
+            }
+            else
+                return $"Download failed with code: {response.StatusCode}";
         }
     }
 }
